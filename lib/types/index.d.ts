8797e33b4110a710ffae3d2f17368e60006b300e
Tarciso Junior <tarciso@nsolucoes.digital>
2024-07-01 17:12:09 -0300

    feat: helpIconFloatShow

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -25,101 +25,35 @@
 /// <reference types="mongoose/types/inferschematype" />
 export { default as mongoose } from "mongoose";
 export { Workspace, WorkspaceSchemaType } from "./models/workspace";
-export {
-  WorkspaceConfig,
-  WorkspaceConfigSchemaType,
-} from "./models/workspaceConfig";
+export { WorkspaceConfig, WorkspaceConfigSchemaType, } from "./models/workspaceConfig";
 export { User, UserSchemaType } from "./models/user";
-export {
-  ContactAction,
-  ContactActionSchemaType,
-  ContactActionSchema,
-} from "./models/contactAction";
-export {
-  TransmissionControl,
-  TransmissionControlSchema,
-  TransmissionControlSchemaType,
-} from "./models/transmissionControl";
+export { ContactAction, ContactActionSchemaType, ContactActionSchema, } from "./models/contactAction";
+export { TransmissionControl, TransmissionControlSchema, TransmissionControlSchemaType, } from "./models/transmissionControl";
 export { Transmission } from "./models/transmission";
-export {
-  AnswerSchema,
-  QuestionSchema,
-  Questionnaire,
-  QuestionnaireSchema,
-  QuestionnaireSchemaType,
-} from "./models/questionnaire";
+export { AnswerSchema, QuestionSchema, Questionnaire, QuestionnaireSchema, QuestionnaireSchemaType, } from "./models/questionnaire";
 export { Plan, PlanSchemaType } from "./models/plan";
 export { Notification, NotificationSchemaType } from "./models/notification";
-export {
-  MessageSchedule,
-  MessageScheduleSchema,
-  MessageScheduleSchemaType,
-} from "./models/messageSchedule";
+export { MessageSchedule, MessageScheduleSchema, MessageScheduleSchemaType, } from "./models/messageSchedule";
 export { Macro, MacroSchema, MacroSchemaType } from "./models/macro";
-export {
-  MacroGroup,
-  MacroGroupSchema,
-  MacroGroupSchemaType,
-} from "./models/macroGroup";
-export {
-  LeadHistory,
-  LeadHistorySchema,
-  LeadHistorySchemaType,
-} from "./models/leadHistory";
+export { MacroGroup, MacroGroupSchema, MacroGroupSchemaType, } from "./models/macroGroup";
+export { LeadHistory, LeadHistorySchema, LeadHistorySchemaType, } from "./models/leadHistory";
 export { Logs, LogsSchemaType } from "./models/logs";
 export { LiveChat, ChatSchemaType } from "./models/livechat";
-export {
-  IntegrationLogs,
-  IntegrationLogsSchema,
-  IntegrationLogsSchemaType,
-} from "./models/integrationLogs";
-export {
-  Integration,
-  IntegrationSchema,
-  IntegrationSchemaType,
-} from "./models/integration";
+export { IntegrationLogs, IntegrationLogsSchema, IntegrationLogsSchemaType, } from "./models/integrationLogs";
+export { Integration, IntegrationSchema, IntegrationSchemaType, } from "./models/integration";
 export { Group, GroupSchema, GroupSchemaType } from "./models/group";
-export {
-  FunnelSales,
-  FunnelSalesSchema,
-  FunnelSalesSchemaType,
-} from "./models/funnelSales";
+export { FunnelSales, FunnelSalesSchema, FunnelSalesSchemaType, } from "./models/funnelSales";
 export { FlowEvent, FlowEventsSchemaType } from "./models/FlowEvent";
 export { Coupon, CouponSchema, CouponSchemaType } from "./models/coupon";
 export { Contact, ContactSchema, ContactSchemaType } from "./models/contact";
-export {
-  ContactAnnotation,
-  ContactAnnotationSchema,
-  ContactAnnotationSchemaType,
-} from "./models/contactAnnotation";
+export { ContactAnnotation, ContactAnnotationSchema, ContactAnnotationSchemaType, } from "./models/contactAnnotation";
 export { ChatMessage, MessageSchemaType } from "./models/chatmessage";
-export {
-  BusinessGroup,
-  BusinessGroupSchema,
-  BusinessGroupSchemaType,
-} from "./models/businessGroup";
-export {
-  AwaitingResponse,
-  AwaitingResponseSchemaType,
-} from "./models/AwaitingResponse";
-export {
-  FlowLogs,
-  FlowLogsSchema,
-  FlowLogsSchemaType,
-} from "./models/flowLogs";
+export { BusinessGroup, BusinessGroupSchema, BusinessGroupSchemaType, } from "./models/businessGroup";
+export { AwaitingResponse, AwaitingResponseSchemaType, } from "./models/AwaitingResponse";
+export { FlowLogs, FlowLogsSchema, FlowLogsSchemaType, } from "./models/flowLogs";
 export { LabelConfigs, LabelConfigsSchemaType } from "./models/labelConfigs";
-export {
-  Platform,
-  PlatformsSchema,
-  PlatformsSchemaType,
-} from "./models/platforms";
-export {
-  PlatformLogs,
-  PlatformLogsSchema,
-  PlatformLogsSchemaType,
-} from "./models/platformsLogs";
-export {
-  ContactImportSchemaType,
-  ContactsImport,
-  ContactsImportSchema,
-} from "./models/contactsImport";
+export { Platform, PlatformsSchema, PlatformsSchemaType, } from "./models/platforms";
+export { PlatformLogs, PlatformLogsSchema, PlatformLogsSchemaType, } from "./models/platformsLogs";
+export { Resource, ResourceSchema, ResourceSchemaType, } from "./models/resource";
+export { ResourceLogs, ResourceLogsSchema, ResourceLogsSchemaType, } from "./models/resourceLogs";
+export { ContactImportSchemaType, ContactsImport, ContactsImportSchema, } from "./models/contactsImport";
